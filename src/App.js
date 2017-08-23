086e97c1db13963d8d615bc848eb9711dc594b66
nadavperetz <nadavperetz@gmail.com>
2017-08-23 13:12:50 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class ContactList extends React.Component {
+    render(){
+        const people = this.props.contacts;
+        return <ol>
+            {people.map(person => (
+                <li key={person.name}>{person.name}</li>
+            ))}
+        </ol>
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+              <ContactList contacts = { [
+                  {name: "Michael"},
+                  {name: "Ryan"},
+                  {name: "Tyler"},
+                  {name: "Nadav"}
+              ]}/>
+              <ContactList contacts = { [
+                  {name: "Michael"},
+                  {name: "Ryan"},
+                  {name: "Tyler"},
+                  {name: "Nadav"}
+              ]}
+            />
+    </div>
+    );
+    }
+}
+
+export default App;
